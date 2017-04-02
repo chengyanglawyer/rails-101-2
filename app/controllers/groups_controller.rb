d7ce951caa3529cef61aecc781f73f7add310936
“程杨” <474880453@qq.com>
2017-04-02 18:57:42 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,9 +13,13 @@ end
  end
  def create
    @group = Group.new(group_params)
-   @group.save
 
-     redirect_to groups_path
+   if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+
  end
    def update
     @group = Group.find(params[:id])
@@ -35,5 +39,5 @@ end
 
  def group_params
    params.require(:group).permit(:title, :description)
-end
+ end
 end
